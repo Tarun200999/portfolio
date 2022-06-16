860246ad6837205a68dcf068f9f00b29cd0b68ac
Tarun200999 <tk4977163@gmail.com>
2022-06-16 17:43:06 +0530

    Last Update 16 June 2022

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -21,6 +21,60 @@ function Experience() {
                   <div className="col-sm-8">
                     <div className="exp_data">
                       <h2>Kylo Apps</h2>
+                      <h4>Role : Full Stack Developer Intern</h4>
+                      <h4>Job description</h4>
+                      <p>
+                        I am working on various projects . Some are from scratch
+                        amd some from middle. My work is to make design database
+                        scheama , bussiness logic of the Mobile/ Web application
+                        and creating RESTAPI in EXPRESS , NODEJS , SQL / NOSQL .
+                        <br></br>
+                        <strong>Projects</strong>
+                        <ul>
+                          <li>Employee Data Managment </li>
+                          <li>Investor Portfolio </li>
+                          <li>Agriculter Auction Managment System </li>
+                          <li>Online Course Providing Application </li>
+                          <li>Fundraiser Application </li>
+                        </ul>
+                        <strong>Learnings</strong>
+                        <ul>
+                          <li>Authentication & AUthorization </li>
+                          <li>
+                            Payment Gatway Integration Paytm , Razorpay &
+                            CCavenue
+                          </li>
+                          <li>Schema Design </li>
+                          <li>API Optimization and many more...</li>
+                        </ul>
+                      </p>
+
+                      <h4>
+                        Certificate : <a href="">Here</a>
+                      </h4>
+                      <h4>
+                        LOR :<a href="">Here</a>{' '}
+                      </h4>
+                      <h4>Duration : January 2022 to OnGoing</h4>
+                      <a href="https://kyloapps.com/">Kylo Apps</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4"></div>
+          </div>
+          <br></br>
+          <div className="row">
+            <div className="col-sm-8">
+              <div className="exp_content">
+                <div className="row">
+                  <div className="col-sm-4">
+                    <img src={Kylo} />
+                  </div>
+                  <div className="col-sm-8">
+                    <div className="exp_data">
+                      <h2>Kylo Apps</h2>
                       <h4>Role : Backend Developer Intern</h4>
                       <h4>Job description</h4>
                       <p>
@@ -55,7 +109,7 @@ function Experience() {
                       <h4>
                         LOR :<a href="">Here</a>{' '}
                       </h4>
-                      <h4>Duration : August 2021 to OnGoing</h4>
+                      <h4>Duration : August to Dec 2021</h4>
                       <a href="https://kyloapps.com/">Kylo Apps</a>
                     </div>
                   </div>
